8dced693dfc545a6654059f94ba1906f3ffcba46
Abirami-Murugan <mabiramiscet@gmail.com>
2022-10-17 03:53:45 +0530

    interview practice

---
+++ Parent.java
@@ -0,0 +1,15 @@
+public abstract class Parent2
+{
+public static void main(String[] args)
+{
+// It cannot be instantiated
+}
+public abstract void study();
+//Method Overriding:
+
+public void grow()
+{
+System.out.println("Energetic");
+}
+
+}
